0c63c4b087a4f12f5bdc33a5041c664606cefa37
prayas rode <prode1@uncc.edu>
2018-10-19 12:53:47 -0400

    first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,43 @@
+import { Recipe } from './recipe.model';
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel', 'Very tasty Schnitzel',
+      // tslint:disable-next-line:max-line-length
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient('French fries', 20)
+      ]
+      ),
+      new Recipe(
+        'Big fat Burger', 'Very very very fat burger',
+      // tslint:disable-next-line:max-line-length
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+      )
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
